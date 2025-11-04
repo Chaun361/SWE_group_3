6be49d4582ac71f9e11177c0eb6550bac9899be0
Xplosion97 <nawapat.thum@dome.tu.ac.th>
2025-11-05 05:52:56 +0700

    OrderItemModel Debugged

--- demo/src/main/java/com/example/demo/Order/model/OrderItemModel.java
+++ demo/src/main/java/com/example/demo/Order/model/OrderItemModel.java
@@ -65,11 +65,11 @@ public class OrderItemModel {
         Quantity = quantity;
     }
 
-    public double getPricePerUnit() { // <-- แก้ Type และชื่อเมธอด
+    public double getUnitPrice() { // <-- แก้ Type และชื่อเมธอด
         return PricePerUnit;
     }
 
-    public void setPricePerUnit(double pricePerUnit) { // <-- แก้ Type และชื่อเมธอด
+    public void setUnitPrice(double pricePerUnit) { // <-- แก้ Type และชื่อเมธอด
         PricePerUnit = pricePerUnit;
     }
 }
